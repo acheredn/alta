a95019c646812c873c4132a1f34aec5e88b7904e
acheredn <73703578+acheredn@users.noreply.github.com>
2022-09-28 10:23:46 -0500

    setting up basic pages and navbar
    please run npm update --save before

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import logo from "./logo.svg";
-import "./App.css";
-
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route}
+    from 'react-router-dom';
+import Home from './pages';
+import About from './pages/about';
+import Blogs from './pages/blogs';
+import SignUp from './pages/signup';
+import Contact from './pages/contact';
+  
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        header className = "App-header" >
-        <
-        img src = { logo }
-        className = "App-logo"
-        alt = "logo" / >
-        <
-        p >
-        Edit < code > src / App.js < /code> and HELLO ALTA GROUP{" "} <
-        /p>{" "} <
-        a className = "App-link"
-        href = "https://reactjs.org"
-        target = "_blank"
-        rel = "noopener noreferrer" >
-        BOOM BOOM BOOM { " " } <
-        /a>{" "} <
-        /header>{" "} <
-        /div>
-    );
+return (
+    <Router>
+    <Navbar />
+    <Routes>
+        <Route path='/' exact element={<Home />} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/blogs' element={<Blogs/>} />
+        <Route path='/sign-up' element={<SignUp/>} />
+    </Routes>
+    </Router>
+);
 }
-
+  
 export default App;
