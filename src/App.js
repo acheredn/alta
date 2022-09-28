c60f9aa96785f6ca3201840e3b812f3c170e2259
Tri <hungnd@newwave.vn>
2022-09-28 10:08:28 -0500

    e

--- src/App.js
+++ src/App.js
@@ -2,27 +2,24 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        header className = "App-header" >
-        <
-        img src = { logo }
-        className = "App-logo"
-        alt = "logo" / >
-        <
-        p >
-        Edit < code > src / App.js < /code> and HELLO ALTA GROUP{" "} <
-        /p>{" "} <
-        a className = "App-link"
-        href = "https://reactjs.org"
-        target = "_blank"
-        rel = "noopener noreferrer" >
-        BOOM BOOM BOOM { " " } <
-        /a>{" "} <
-        /header>{" "} <
-        /div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>     src / App.js </code> and HELLO ALTA GROUP{" "}
+        </p>{" "}
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          BOOM BOOM BOOM{" "}
+        </a>{" "}
+      </header>{" "}
+    </div>
+  );
 }
 
 export default App;
