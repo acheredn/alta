9384f6be85a41793d09769621b9596d44b09a128
Tri <hungnd@newwave.vn>
2022-09-28 10:14:42 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 
 function App() {
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>     src / App.js </code> and HELLO ALTA GROUP{" "}
+          Edit <code>src / App.js </code> and HELLO ALTA GROUP{" "}
         </p>{" "}
         <a
           className="App-link"
@@ -20,6 +26,13 @@ function App() {
       </header>{" "}
     </div>
   );
+}
+
+async function getCities(db) {
+  const citiesCol = collection(db, 'cities');
+  const citySnapshot = await getDocs(citiesCol);
+  const cityList = citySnapshot.docs.map(doc => doc.data());
+  return cityList;
 }
 
 export default App;
