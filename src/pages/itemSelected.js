4d15c32ef46e2c3daa4bbbe79fecdb864a90d08f
Tri <hungnd@newwave.vn>
2022-10-02 13:48:32 -0500

    update

--- src/pages/itemSelected.js
+++ src/pages/itemSelected.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Button from "@mui/material/Button";
+
 
 const ItemSelected = () => {
-return (
-	<h1>ITEM SELECTED 
-		<p> <Button variant="contained"> Contact Seller</Button> </p> </h1>
-);
+	return (
+		<h1>ITEM SELECTED
+		</h1>
+	);
 };
+
 
 export default ItemSelected;
