bce59e3db9dabedf8f914f9cada6fd3575552901
acheredn <73703578+acheredn@users.noreply.github.com>
2022-10-02 14:19:38 -0500

    started working on listItem page

--- src/pages/itemList.js
+++ src/pages/itemList.js
@@ -1,11 +1,90 @@
-import React from 'react';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-const ItemList = () => {
-return (
-	<div>
-	<h1>ITEMS LIST</h1>
-	</div>
-);
-};
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-export default ItemList;
+const theme = createTheme();
+
+export default function ItemList() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <main>
+        {/* Hero unit */}
+        <Box
+          sx={{
+            pt: 8,
+            pb: 2,
+          }}
+        >
+          <Container maxWidth="sm">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="text.primary"
+              gutterBottom
+            >
+              Items List
+            </Typography>
+            <Stack
+              sx={{ pt: 4 }}
+              direction="row"
+              spacing={2}
+              justifyContent="center"
+            >
+              <Button variant="contained">Search for item</Button>
+              <Button variant="outlined">Add an item</Button>
+            </Stack>
+          </Container>
+        </Box>
+
+        <Container sx={{ py: 8 }} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card
+                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                >
+                  <CardMedia
+                    component="img"
+                    sx={{
+                      pt: '0%',
+                    }}
+                    image="http://placeimg.com/640/480/animals"
+                    alt="random"
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Gray croptop with blue shorts
+                    </Typography>
+                    <Typography>
+                      This is brief item description
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">View</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+    </ThemeProvider>
+  );
+}
