bc047fcef8a0b96fe808321bd7e32acb8a620a67
Tri <hungnd@newwave.vn>
2022-10-02 14:05:43 -0500

    update button

--- src/pages/itemSelected.js
+++ src/pages/itemSelected.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
 
 
 const ItemSelected = () => {
+	const Button = styled.button`
+  background-color: black;
+  color: white;
+  font-size: 20px;
+  padding: 10px 60px;
+  border-radius: 5px;
+  margin: 10px 0px;
+  cursor: pointer;
+`;
 	return (
-		<h1>ITEM SELECTED
-		</h1>
+		<><h1>ITEM SELECTED
+		</h1><Button onClick={sayHello}>Contact me</Button></>
+
 	);
 };
+function sayHello() {
+	alert('You clicked me!');
+}
+
+
+
+// Usage
 
 
 export default ItemSelected;
