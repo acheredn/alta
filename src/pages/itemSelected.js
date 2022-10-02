47a09e1c881f8dfc0d351268b8540a4ab300219a
acheredn <73703578+acheredn@users.noreply.github.com>
2022-10-02 13:41:25 -0500

    images folder put in /src

--- src/pages/itemSelected.js
+++ src/pages/itemSelected.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Button from "@mui/material/Button";
 
 const ItemSelected = () => {
 return (
-	<h1>ITEM SELECTED</h1>
+	<h1>ITEM SELECTED 
+		<p> <Button variant="contained"> Contact Seller</Button> </p> </h1>
 );
 };
 
