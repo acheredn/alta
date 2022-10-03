1aabed4d2d2310dde1f5a85c641cb8ba89516295
Lorena <lescobos@macalester.edu>
2022-10-03 10:41:12 -0500

    Index title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+// import Link from '@mui/material/Link';
+import { Link } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const Home = () => {
 return (
-	<div>
-	<h1>WELCOME PAGE</h1>
-	</div>
+	<Container maxWidth="sm">
+        <Typography
+        component="h1"
+        variant="h2"
+        align="center"
+        color="text.primary"
+        gutterBottom
+        >
+        CLOTHING SWAP 
+    </Typography>
+	</Container>
 );
 };
 
