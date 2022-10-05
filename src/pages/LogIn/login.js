7ee3caafb3d86da7e611fee2b6d95ef812a453ee
aktran3 <kztran0235@gmail.com>
2022-10-04 19:13:00 -0500

    added sign in with google buttons in login and sign in, sign up connected

--- src/pages/LogIn/login.js
+++ src/pages/LogIn/login.js
@@ -1,4 +1,5 @@
 // template from https://github.com/mui/material-ui/blob/v5.10.7/docs/data/material/getting-started/templates/sign-in/SignIn.js
+
 import React from "react";
 import TextField from '@mui/material/TextField';
 import Avatar from '@mui/material/Avatar';
@@ -24,68 +25,79 @@ export default function SignIn() {
 	};
 	
 	return (
-		<Container component="main" maxWidth="xs">
+		
+		<body>
+			<Container component="main" maxWidth="xs">
 			<CssBaseline />
-			<Box
-			sx={{
-				marginTop: 8,
-				display: 'flex',
-				flexDirection: 'column',
-				alignItems: 'center',
-			}}
-			>
-			<Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-			</Avatar>
-			<Typography component="h1" variant="h5">
-				Sign in With Your Macalester Email
-			</Typography>
-			<Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-				<TextField
-				margin="normal"
-				required
-				fullWidth
-				id="email"
-				label="Email Address"
-				name="email"
-				autoComplete="email"
-				autoFocus
-				/>
-				<TextField
-				margin="normal"
-				required
-				fullWidth
-				name="password"
-				label="Password"
-				type="password"
-				id="password"
-				autoComplete="current-password"
-				/>
-				<FormControlLabel
-				control={<Checkbox value="remember" color="primary" />}
-				label="Remember me"
-				/>
-				<Button
-				type="submit"
-				fullWidth
-				variant="contained"
-				sx={{ mt: 3, mb: 2 }}
+				<Box
+				sx={{
+					marginTop: 8,
+					display: 'flex',
+					flexDirection: 'column',
+					alignItems: 'center',
+				}}
 				>
-				Sign In
-				</Button>
-				<Grid container>
-				{/* <Grid item xs>
-					<Link href="#" variant="body2">
-					Forgot password?
-					</Link>
-				</Grid> */}
-				<Grid item>
-					<Link href="/sign-up" variant="body2">
-					{"Don't have an account? Sign Up"}
-					</Link>
-				</Grid>
-				</Grid>
-			</Box>
-			</Box>
-		</Container>
+				<Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+				</Avatar>
+				<Typography component="h1" variant="h5">
+					Sign in With Your Macalester Email
+				</Typography>
+				<Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+					<TextField
+					margin="normal"
+					required
+					fullWidth
+					id="email"
+					label="Email Address"
+					name="email"
+					autoComplete="email"
+					autoFocus
+					/>
+					<TextField
+					margin="normal"
+					required
+					fullWidth
+					name="password"
+					label="Password"
+					type="password"
+					id="password"
+					autoComplete="current-password"
+					/>
+					<FormControlLabel
+					control={<Checkbox value="remember" color="primary" />}
+					label="Remember me"
+					/>
+					<Button
+					type="submit"
+					fullWidth
+					variant="contained"
+					sx={{ mt: 3, mb: 2 }}
+					>
+					Sign In
+					</Button>
+					<Button
+					type="submit"
+					fullWidth
+					variant="contained"
+					sx={{ mt: 3, mb: 2 }}
+					>
+					Sign In With Google
+					</Button>
+					<Grid container>
+					<Grid item xs>
+						<Link href="#" variant="body2">
+						Forgot password?
+						</Link>
+					</Grid>
+					<Grid item>
+						<Link href="/sign-up" variant="body2">
+						{"Don't have an account? Sign Up"}
+						</Link>
+					</Grid>
+					</Grid>
+				</Box>
+				</Box>
+			</Container>
+		</body>
 	)
 };
