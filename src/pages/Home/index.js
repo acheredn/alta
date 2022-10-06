2fae10fcdee4a87ce8d93fdad7877129ed2803c5
aktran3 <kztran0235@gmail.com>
2022-10-06 07:53:39 -0500

    fixed gap in home page nav with cssbaseline, but it looks like there are 2 homes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,7 +3,7 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import bg from '../../images/background.jpg';
 import Box from '@mui/material/Box';
-
+import CssBaseline from '@mui/material/CssBaseline';
 
 
 const Home = () => {
@@ -19,6 +19,7 @@ const Home = () => {
         height: "100vh",
         color: "#f5f5f5"
     }}> 
+    <CssBaseline/>
 	<Container maxWidth="sm">
         <Typography
         component="h1"
