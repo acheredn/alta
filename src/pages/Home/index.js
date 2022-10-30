07c18027193a0d158af7950d609f1c2cbb2098dd
Lorena <lescobos@macalester.edu>
2022-10-30 13:30:27 -0500

    start shopping button

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,6 +5,12 @@ import bg from '../../images/background.jpg';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import CardActions from '@mui/material/CardActions';
+import Card from '@mui/material/Card';
+import { Link } from 'react-router-dom';
+
 
 const theme = createTheme({
     typography: { fontFamily: ["Abril Fatface"]}
@@ -35,6 +41,23 @@ const Home = () => {
         <link
         href="https://fonts.googleapis.com/css2?family=Yellowtail&display=swap"
         rel="stylesheet"/>
+
+        
+        <Stack
+        sx={{ pt: 60 }}
+        direction="row"
+        spacing={2}
+        justifyContent="center">
+       
+        <Button href="/item-list"
+        style={{
+        backgroundColor: "#000000",
+        padding: "18px 36px",
+        fontSize: "18px"
+        }}variant="contained">
+            Start shopping</Button>
+        </Stack>
+        
 
     </Typography>
 	</Container>
