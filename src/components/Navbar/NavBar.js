f900828f51029bb85bf72455be99fd7ba8e65ce4
aktran3 <kztran0235@gmail.com>
2022-11-08 19:18:53 -0600

    added search bar to navbar

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -8,6 +8,10 @@ import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import Typography from '@mui/material/Typography';
 import logout from '../../firebase.js'
+import { styled, alpha } from '@mui/material/styles';
+import SearchIcon from '@mui/icons-material/Search';
+import InputBase from '@mui/material/InputBase';
+
 
 
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout']; // change these as needed
@@ -60,6 +64,48 @@ function Navbar() {
 		setAnchorElUser(null);
 	};
 
+	const Search = styled('div')(({ theme }) => ({
+		position: 'relative',
+		borderRadius: theme.shape.borderRadius,
+		backgroundColor: alpha(theme.palette.common.white, 0.15),
+		'&:hover': {
+		  backgroundColor: alpha(theme.palette.common.white, 0.25),
+		},
+		marginLeft: 0,
+		width: '100%',
+		[theme.breakpoints.up('sm')]: {
+		  marginLeft: theme.spacing(1),
+		  width: 'auto',
+		},
+	  }));
+	  
+	  const SearchIconWrapper = styled('div')(({ theme }) => ({
+		padding: theme.spacing(0, 2),
+		height: '100%',
+		position: 'absolute',
+		pointerEvents: 'none',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+	  }));
+	  
+	  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+		color: 'inherit',
+		'& .MuiInputBase-input': {
+		  padding: theme.spacing(1, 1, 1, 0),
+		  // vertical padding + font size from searchIcon
+		  paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+		  transition: theme.transitions.create('width'),
+		  width: '100%',
+		  [theme.breakpoints.up('sm')]: {
+			width: '12ch',
+			'&:focus': {
+			  width: '20ch',
+			},
+		  },
+		},
+	  }));
+
 	return (
 		<header className={navbarClasses.join(" ")}>
 			<a class="logo" href="/#">SWAP</a>
@@ -70,6 +116,15 @@ function Navbar() {
 				<a href="sign-up">Sign Up</a>
 				<a href="my-items">My Items</a>
 				<a href="test"> Test </a>
+				<Search>
+					<SearchIconWrapper>
+					<SearchIcon />
+					</SearchIconWrapper>
+					<StyledInputBase
+						placeholder="Search…"
+						inputProps={{ 'aria-label': 'search' }}
+					/>
+				</Search>
 				<IconButton onClick={handleOpenUserMenu} sx={{ width: .1 }}>
 					<img src="http://www.clker.com/cliparts/f/a/0/c/1434020125875430376profile-hi.png"></img>
 				</IconButton>
