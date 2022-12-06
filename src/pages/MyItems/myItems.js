2208a255de470ad589762a2f31b6af9d2b1aaf31
Tri <hungnd@newwave.vn>
2022-12-06 13:26:06 -0600

    update working on the firebase data collection

--- src/pages/MyItems/myItems.js
+++ src/pages/MyItems/myItems.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './myItems.css'
 import Item from './Item'
 import { useState, useEffect } from 'react'
-import { collection, query, orderBy, onSnapshot, doc } from "firebase/firestore"
+import { collection, query, orderBy, onSnapshot, doc, getDocs } from "firebase/firestore"
 import { db, auth } from '../../firebase'
 import AddItem from './AddItem'
 import Grid from '@mui/material/Grid';
@@ -12,22 +12,22 @@ export default function ItemsList() {
   const [Items, setItems] = useState([])
 
   /* function to get all Items from firestore in realtime */
-  useEffect(async() => {
+  useEffect(async () => {
     const user = auth.currentUser;
-    if(user){
+    if (user) {
       const usersDocRef = doc(db, "users", user.uid)
       const colRef = collection(usersDocRef, "items")
       const itemColRef = query(colRef, orderBy('created', 'desc'))
-      onSnapshot(itemColRef, (snapshot) => {
-        console.log("hello")
-        setItems(snapshot.docs.map(doc => ({
-          id: doc.id,
-          data: doc.data()
-        })))
-      })
-    }
 
-  }, [])
+      const querySnapshot = await getDocs(itemColRef);
+      // console.log("hello")
+      setItems(querySnapshot.docs.map(doc => ({
+        id: doc.id,
+        data: doc.data()
+      })))
+
+    }
+  }, [Items])
 
   return (
     <div className='ItemManager'>
@@ -38,23 +38,23 @@ export default function ItemsList() {
           Add Item +
         </button>
       </div>
-      
-      <div class = "item_grid">
-      <Grid container spacing={8}>
-        {Items.map((items) => (
-          <Grid key={items} xs={12} sm={6} md={6}>
-            <Item
-              id={items.id}
-              key={items.id}
-              completed={items.data.completed}
-              title={items.data.title}
-              description={items.data.description}
-              image={items.data.image}
-            />
-          </Grid>
-        ))}
 
-      </Grid>
+      <div class="item_grid">
+        <Grid container spacing={8}>
+          {Items.map((items) => (
+            <Grid key={items} xs={12} sm={6} md={6}>
+              <Item
+                id={items.id}
+                key={items.id}
+                completed={items.data.completed}
+                title={items.data.title}
+                description={items.data.description}
+                image={items.data.image}
+              />
+            </Grid>
+          ))}
+
+        </Grid>
       </div>
 
       {openAddModal &&
