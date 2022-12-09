863b498131953d18db5d217b05e89b7ea271c10b
acheredn <73703578+acheredn@users.noreply.github.com>
2022-12-09 10:03:12 -0600

    search page aligned with homepage

--- src/pages/Seach/search.js
+++ src/pages/Seach/search.js
@@ -20,12 +20,14 @@ const searchClient = algoliasearch(
 export default function Search() {
     return (
         <InstantSearch searchClient={searchClient} indexName="items">
+            <div className = "container">
             <h1 >Welcome to Loop Store!</h1>
             <h2 >to push your items to Loop Store, go to My Item page </h2>
             <Configure hitsPerPage={6} />
             <div id="searchbox"> <SearchBox translations={{ placeholder: "Search for items" }} /> </div>
             <div id="hits"> <Hits hitComponent={Hit} /> </div>
             <div id="pagination"> <Pagination /> </div>
+            </div>
         </InstantSearch>
     );
 
