93ae05fe0aef61c3a8c74d7caffc76d13bb49351
Tri <hungnd@newwave.vn>
2022-12-09 10:33:36 -0600

    update css for about us page

--- src/pages/AboutUs/AboutUs.js
+++ src/pages/AboutUs/AboutUs.js
@@ -3,7 +3,7 @@ export default function AboutUs() {
     return (
         <body>
             <h1 >Alta Team</h1>
-            <h2 >We are young, cool, and energetic student care about sustainable fashion! </h2>
+            <h2 >We are young, cool, and passionate students from Macalester College, USA. We care about sustainable fashion and want to build a sustainable world! </h2>
             <div className="video-responsive">
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/A5DQhbrKoB8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
